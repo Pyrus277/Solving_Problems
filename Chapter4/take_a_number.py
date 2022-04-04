59883274c4ce059ec7d15b297eac46bb76c954eb
Perry <pyrus277@gmail.com>
2022-04-04 03:27:58 -0400

    chapter problems

--- Chapter4/take_a_number.py
+++ Chapter4/take_a_number.py
@@ -1,4 +1,15 @@
 #ecoo13r1p1
+# see full promt at https://dmoj.ca/problem/ecoo13r1p1
+# it's a long one.
+
+# bascially we're tracking a system where people waiting take a ticket
+# inputs are the actions of 1. a person taking a new ticket, 2. a person being
+# to, 3. the desk closing, and 4. input to trigger the end of the program.
+# Upon each desk closure, the program should return a tally of 1. the number
+# of people who waited that day, 2. the number still waiting when the desk closed
+# and 3. the next number in the ticket machine.  
+
+
 
 late = 0
 waiting = 0
